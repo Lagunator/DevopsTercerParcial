8c065670dc8a6c66db75935e88ad1220236feb78
Lagunator <102009436+Lagunator@users.noreply.github.com>
2024-12-19 09:09:46 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,8 +2,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-
-# Crear un Security Group para EC2 (permitir SSH y puertos 80, 3000 y 3001)
 resource "aws_security_group" "ec2_sg" {
   name_prefix = "ec2-sg-"
   
@@ -11,7 +9,7 @@ resource "aws_security_group" "ec2_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"] # Permite SSH desde cualquier dirección IPv4
   }
 
   ingress {
@@ -85,7 +83,6 @@ resource "aws_instance" "ec2_instance" {
   }
 }
 
-# Output para mostrar la IP pública de la EC2
 output "ec2_public_ip" {
   value = aws_instance.ec2_instance.public_ip
 }
